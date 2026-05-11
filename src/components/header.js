0d9a50a4e5e7dce3a9ebfd8b4b0e1ffe2e58916c
SpanielOF <danyachaika1@gmail.com>
2026-05-11 20:36:23 +0300

    styles in header
    changed the way styles are used for header

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,21 @@
 
 export const renderHeaderMenu = () => {
     return `
-    <header class="header_container">
+    <header id="header_container" style="
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+
+    width: 100%;
+    padding: 26px 120px;
+
+    background-color: rgba(242, 244, 247, 1);
+    ">
         <a class="header_logo" href="/">
             <img src="/src/assets/icons/logo.png" alt="Logo">
         </a>
-        <ul class="navigation_menu">
+        <ul id="navigation_menu" style="display: flex; gap: 32px">
             <li class="navigation_item">
                 <a href="*" class="nav_link">Home</a>
             </li>
